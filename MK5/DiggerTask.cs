7e22d0c2365e60037578da357143f27488416c4f
Yekateryna Boiko <yekateryna.boyko@gmail.com>
2026-09-01 15:29:37 +0300

    Tasks 1 and 2 from MK5 added to the repository

---
+++ MK5/DiggerTask.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digger
+{
+    // ===== Task 1 =====
+    public class Terrain : ICreature
+    {
+        public string GetImageFileName() => "Terrain.png";
+        public int GetDrawingPriority() => 0;
+        public CreatureCommand Act(int x, int y)
+            => new CreatureCommand { DeltaX = 0, DeltaY = 0 };
+        public bool DeadInConflict(ICreature conflictedObject) => true;
+    }
+
+    // ===== Task 2 =====
+    public class Player : ICreature
+    {
+        public string GetImageFileName() => "Digger.png";
+        public int GetDrawingPriority() => 2;
+        public CreatureCommand Act(int x, int y)
+        {
+            var dx = 0;
+            var dy = 0;
+            switch (Game.KeyPressed)
+            {
+                case Keys.Left: dx = -1;
+                    break;
+                case Keys.Right: dx = 1;
+                    break;
+                case Keys.Up: dy = -1;
+                    break;
+                case Keys.Down: dy = 1;
+                    break;
+            }
+            if (x + dx < 0 || x + dx >= Game.MapWidth)
+                dx = 0;
+
+            if (y + dy < 0 || y + dy >= Game.MapHeight)
+                dy = 0;
+
+            return new CreatureCommand { DeltaX = dx, DeltaY = dy };
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject) => false;
+    }
+}
